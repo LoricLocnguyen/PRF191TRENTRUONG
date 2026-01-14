e1e0dda9d76e1c5c2c9d54be1cdc2e06af47fa8c
LoricLocnguyen <dangdaibithiendai@gmail.com>
2026-01-14 08:19:12 +0700

    bai1

---
+++ slot3/ex1.c
@@ -0,0 +1,28 @@
+
+
+
+
+//  a = 7  , b =2          7+2 = 9         7-2 =5        7*2       7/2
+
+#include<stdio.h>
+int main(){
+	int a = 7 ;
+	int b = 2 ;
+	int tong = a +b ;
+	int hieu = a- b ;
+	int nhan  = a * b ; 
+	float  chia = (float)a/b  ;
+	printf(" a la %d\n  ", a  );
+	printf(" b la %d\n  ",  b );
+	printf(" a +b = %d\n " , tong );
+	printf(" a-b = %d\n ",hieu );
+	printf(" a*b = %d\n ", nhan );
+	printf(" a/b = %.2f\n ",chia );
+
+	
+	
+	
+	
+	
+	
+}
